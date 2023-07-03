e9af417c729c86a086b96f716ebe873ee8798d01
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-07-03 11:44:58 +0530

    added timestamp column input in config

--- src/components/config/ConfigEditor.tsx
+++ src/components/config/ConfigEditor.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import { DataSourceHttpSettings } from '@grafana/ui';
+import React, { useState, useEffect } from 'react';
+import { DataSourceHttpSettings, Field, InlineLabel, Input } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { MyDataSourceOptions } from '../../types';
+import { css } from '@emotion/css';
 
 interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
 
 export function ConfigEditor(props: Props) {
   const { onOptionsChange, options } = props;
+
+  const [timestampName, setTimestampName] = useState('');
+
+  useEffect(() => {
+    const timestamp_column = options?.jsonData?.timestamp_column || '_timestamp';
+    if (timestamp_column) {
+      setTimestampName(timestamp_column);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const updateTimestampName = (name: string) => {
+    if (name === '') {
+      name = '_timestamp';
+    }
+    setTimestampName(name);
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...options.jsonData,
+        timestamp_column: name,
+      },
+    });
+  };
 
   return (
     <div>
@@ -15,7 +41,27 @@ export function ConfigEditor(props: Props) {
         dataSourceConfig={options}
         showAccessOptions={false}
         onChange={onOptionsChange}
-      />
+      ></DataSourceHttpSettings>
+      <div className="page-heading">OpenObserve details</div>
+      <div
+        className={css`
+          display: flex;
+          align-items: start;
+          padding-right: 1rem;
+        `}
+      >
+        <InlineLabel className="width-10">Time field name</InlineLabel>
+        <Field
+          invalid={timestampName === ''}
+          error={timestampName === '' ? 'Timestamp Field Name cannot be empty' : ''}
+        >
+          <Input
+            className="width-18"
+            value={timestampName}
+            onChange={(e) => updateTimestampName(e.currentTarget.value)}
+          />
+        </Field>
+      </div>
     </div>
   );
 }
